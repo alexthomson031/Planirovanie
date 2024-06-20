82fc299e580e1da0055b795e4c6636a4c101e1dd
Netotest <athomson@mail.ru>
2024-06-20 17:52:58 +0300

    Initial commit

---
+++ src/main/java/Epic.java
@@ -0,0 +1,24 @@
+import java.util.Objects;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id); // вызов родительского конструктора
+        this.subtasks = subtasks; // заполнение своих полей
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        Epic[] result = new Epic[0];
+        for (String subtusk : subtasks) {
+            if (Objects.equals(subtusk, query))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ype() {
+        SimpleTask simple5 = new SimpleTask(5, "Позвонить родителям 5123312");
+
+        Todos todos = new Todos();
+
+        todos.add(simple5);
+
+        Task[] expected = {simple5};
+        Task[] actual = todos.search("Позвонить родителям 5123312");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
